4c5a9ee0ff8eafbb2c027896af46567080c2d371
Harindi Supunsara <supunsara.harindi@gmail.com>
2017-06-08 13:36:00 +0530

    Reload branch 22

---
+++ OpenQbit.InventorySystem.git/OpenQbit.Inventory.Service.WebApi/Controllers/Api/ApiItemController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using OpenQbit.Inventory.Service.WebApi.Models.Api;
+
+namespace OpenQbit.Inventory.Service.WebApi.Controllers.Api
+{
+    public class ApiItemController:ApiController
+    {
+        public ApiItem Get(int ID)
+        {
+            ApiItem item = new ApiItem
+            {
+                ID = 1,
+                Name = "Dhal",
+                Active = true,
+                reOrder=10
+            };
+            return item;  
+        }
+
+        public List<ApiItem> GetList()
+        {
+            List<ApiItem> itemList = new List<ApiItem>();
+
+            ApiItem item1 = new ApiItem
+            {
+                ID = 1,
+                Name = "Dhal",
+                Active = true,
+                reOrder = 10
+            };
+
+            ApiItem item2 = new ApiItem
+            {
+                ID = 2,
+                Name = "Rice",
+                Active = true,
+                reOrder = 25
+            };
+
+            ApiItem item3 = new ApiItem
+            {
+                ID = 3,
+                Name = "Soap",
+                Active = true,
+                reOrder = 10
+            };
+
+            itemList.Add(item1);
+            itemList.Add(item2);
+            itemList.Add(item3);
+
+            return itemList;
+        }
+    }
+}
---
+++ OpenQbit.InventorySystem.git/OpenQbit.Inventory.Service.WebApi/Controllers/Api/ApiLocationController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using OpenQbit.Inventory.Service.WebApi.Models.Api;
+
+namespace OpenQbit.Inventory.Service.WebApi.Controllers.Api
+{
+    public class ApiLocationController: ApiController
+    {
+        public ApiLocation Get(int ID)
+        {
+            ApiLocation location = new ApiLocation
+            {
+                ID=1,
+                Name="Branch"
+            };
+
+            return location;
+        }
+
+        public List<ApiLocation> GetList()
+        {
+            List<ApiLocation> locationList = new List<ApiLocation>();
+
+            ApiLocation location1 = new ApiLocation
+            {
+                ID = 1,
+                Name = "Branch"
+            };
+
+            ApiLocation location2 = new ApiLocation
+            {
+                ID = 2,
+                Name = "Head Quatest"
+            };
+
+            ApiLocation location3 = new ApiLocation
+            {
+                ID = 3,
+                Name = "Ware House"
+            };
+
+            locationList.Add(location1);
+            locationList.Add(location2);
+            locationList.Add(location3);
+
+            return locationList;
+        }
+    }
+}
