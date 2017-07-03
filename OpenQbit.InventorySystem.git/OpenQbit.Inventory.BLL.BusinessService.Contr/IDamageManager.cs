500ca8288e25f85e93c3fb1f8c2ebf2c3e108ebe
Harindi Supunsara <supunsara.harindi@gmail.com>
2017-07-03 10:18:19 +0530

    Create 3 interface and implement 3 class in BLL.

--- OpenQbit.InventorySystem.git/OpenQbit.Inventory.BLL.BusinessService.Contr/IDamageManager.cs
+++ OpenQbit.InventorySystem.git/OpenQbit.Inventory.BLL.BusinessService.Contr/IDamageManager.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQbit.Inventory.Common.Models;
 
 namespace OpenQbit.Inventory.BLL.BusinessService.Contr
 {
-    public class Class1
+    public interface IDamageManager
     {
+        bool RecoredDamage(Damage damage);
     }
 }
