dfedac880926a263014a55b45e561b2d8a496be6
Anjana Thrishakya <anjanathrishakya@gmail.com>
2017-07-03 16:04:30 +0530

    change customerManager

--- OpenQbit.InventorySystem.git/OpenQbit.Inventory.BLL.BusinessService/CustomerManager.cs
+++ OpenQbit.InventorySystem.git/OpenQbit.Inventory.BLL.BusinessService/CustomerManager.cs
@@ -51,7 +51,7 @@ namespace OpenQbit.Inventory.BLL.BusinessService
         {
             _logger.LogError("");
 
-            return _repository.FindById<Customer>(id);
+            return _repository.FindByID<Customer>(id);
         }
 
         public List<Customer> GetAllCustomers()
