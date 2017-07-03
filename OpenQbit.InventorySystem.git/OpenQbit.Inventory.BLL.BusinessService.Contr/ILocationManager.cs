04fa0f67d35b64a94e557c709b47054e9d126ad0
Anjali Pavithra <anjalipavithra96@gmail.com>
2017-07-03 10:27:24 -0700

    Create three interfaces and three classes in BLL

---
+++ OpenQbit.InventorySystem.git/OpenQbit.Inventory.BLL.BusinessService.Contr/ILocationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Inventory.Common.Models;
+
+namespace OpenQbit.Inventory.BLL.BusinessService.Contr
+{
+    public interface ILocationManager
+    {
+        bool RecordLocation(Location location);
+    }
+}
---
+++ OpenQbit.InventorySystem.git/OpenQbit.Inventory.BLL.BusinessService/LocationManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Inventory.Common.Models;
+using OpenQbit.Inventory.Common.Utils.Logs;
+using OpenQbit.Inventory.DAL.DataAccess.Contr;
+using OpenQbit.Inventory.BLL.BusinessService.Contr;
+using Microsoft.Practices.Unity;
+
+namespace OpenQbit.Inventory.BLL.BusinessService
+{
+    public class LocationManager : ILocationManager
+    {
+        private IRepository _repository;
+
+        private ILogger _log;
+
+        [InjectionConstructor]
+        public LocationManager(IRepository repository, ILogger log)
+        {
+            this._repository = repository;
+            this._log = log;
+        }
+
+        [Dependency]
+        public IRepository Repository
+        {
+            get { return _repository; }
+            set { _repository = value; }
+        }
+
+        [Dependency]
+        public ILogger logger
+        {
+            get { return _log; }
+            set { _log = value; }
+        }
+
+        [InjectionMethod]
+        public void SetRepository(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool RecordLocation(Location location)
+        {
+            _log.LogError("");
+            return _repository.Create<Location>(location);
+        }
+    }
+}
