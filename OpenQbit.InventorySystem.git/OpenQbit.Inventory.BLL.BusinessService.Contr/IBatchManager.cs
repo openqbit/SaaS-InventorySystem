963a1d7e4ec0dffe5e49838a8e7b6eb22a251b45
Anjana Thrishakya <anjanathrishakya@gmail.com>
2017-07-03 10:52:47 +0530

    Create loose class

---
+++ OpenQbit.InventorySystem.git/OpenQbit.Inventory.BLL.BusinessService.Contr/IBatchManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Inventory.Common.Models;
+
+namespace OpenQbit.Inventory.BLL.BusinessService.Contr
+{
+    public interface IBatchManager
+    {
+        bool RecoredBatch(Batch batch);
+    }
+}
